91de9ef1222435f35f32efccc9424b8ab93f8ad0
z-apply <admin@zapply.jobs>
2026-01-28 10:32:38 +0500

    more resources update

--- .github/scripts/job-fetcher/readme-generator.js
+++ .github/scripts/job-fetcher/readme-generator.js
@@ -557,6 +557,8 @@ ${generateJobTable(currentJobs)}
   <a href="https://github.com/zapplyjobs/New-Grad-Hardware-Engineering-Jobs-2026"><img src="images/repo-hej.png" alt="Hardware Engineering Jobs" height="40"></a>
 </p>
 <p align="center">
+  <a href="https://github.com/zapplyjobs/New-Grad-Jobs-2026"><img src="images/repo-ngj.png" alt="New Grad Jobs" height="40"></a>
+  &nbsp;&nbsp;
   <a href="https://github.com/zapplyjobs/New-Grad-Nursing-Jobs-2026"><img src="images/repo-nsj.png" alt="Nursing Jobs" height="40"></a>
   &nbsp;&nbsp;
   <a href="https://github.com/zapplyjobs/resume-samples-2026"><img src="images/repo-rss.png" alt="Resume Samples" height="40"></a>
